a5d0751eca7c720479a1be86a57ab1b536fef66c
JIJIClyde <eric3285@naver.com>
2021-09-17 23:25:55 +0900

    oneroom index 설명

--- oneroom/models.py
+++ oneroom/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+class oneroom_post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(blank=True, null=True)
+    title = models.TextField(blank=True, null=True)
+    gas_type = models.TextField(blank=True, null=True)
--- oneroom/views.py
+++ oneroom/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import *
 
 
 # Create your views here.
 
 def index(request):
-    return render(request)
+    data = oneroom_post.objects.order_by('create_date')
+    context = {'data_list': data}
+    return render(request, 'oneroom/index.html', context)
