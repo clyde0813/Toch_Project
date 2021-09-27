4a524c6e55e9e12bd75b7c9d8334a3c9949c1d24
unknown <drericcity@gmail.com>
2021-09-27 13:45:14 +0900

    added usedtrade, community

--- Toch/urls.py
+++ Toch/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('oneroom', include('oneroom.urls')),
+    path('', include('oneroom.urls')),
 ]
