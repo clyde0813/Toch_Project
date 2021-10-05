9f6c3d60751a55a9265188102a1f46b7a7610f5a
clyde0813 <kakao@gmail.com>
2021-10-05 22:05:07 +0900

    usedtrade index 반복문 추가

--- usedtrade/forms.py
+++ usedtrade/forms.py
@@ -6,8 +6,9 @@ from .models import *
 class PostForm(forms.ModelForm):
     class Meta:
         model = usedtrade_post
-        fields = ['title', 'content']
+        fields = ['title', 'price', 'content']
         labels = {
             'title': '제목',
+            'price': '가격',
             'content': '내용',
         }
--- usedtrade/views.py
+++ usedtrade/views.py
@@ -6,11 +6,18 @@ from .forms import *
 
 # Create your views here.
 def index(request):
-    return render(request, 'usedtrade/index.html')
+    data = usedtrade_post.objects.order_by('created_date')
+    context = {"data": data}
+    return render(request, 'usedtrade/index.html', context)
 
 
 def detail(request, num):
-    return render(request, 'usedtrade/detail.html')
+    if usedtrade_post.objects.filter(id=num):
+        data = usedtrade_post.objects.filter(id=num)
+        context = {'data': data}
+        return render(request, 'usedtrade/detail.html', context)
+    else:
+        return render(request, '404.html')
 
 
 def create_post(request):
@@ -18,7 +25,7 @@ def create_post(request):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
-            post.create_date = timezone.now()
+            post.created_date = timezone.now()
             post.save()
             return redirect('usedtrade:detail', post.id)
     else:
