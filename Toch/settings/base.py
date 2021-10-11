3da90f7584e718d519f40e4616925de282447a15
clyde0813 <kakao@gmail.com>
2021-10-11 21:47:20 +0900

    logger 추가

--- Toch/settings/base.py
+++ Toch/settings/base.py
@@ -106,6 +106,68 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# 로깅설정
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+    'formatters': {
+        'django.server': {
+            '()': 'django.utils.log.ServerFormatter',
+            'format': '[{server_time}] {message}',
+            'style': '{',
+        },
+    },
+    'standard': {
+        'format': '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+    },
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        },
+        'django.server': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'django.server',
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler'
+        },
+        'file': {
+            'level': 'INFO',
+            'filters': ['require_debug_false'],
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': BASE_DIR / 'logs/Toch.log',
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'standard',
+        },
+    },
+
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'mail_admins', 'file'],
+            'level': 'INFO',
+        },
+        'django.server': {
+            'handlers': ['django.server'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
+
 # ASGI_APPLICATION = 'Toch.routing.application'
 #
 # CHANNEL_LAYERS = {
@@ -119,15 +181,6 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
-
-# mokpo.ac.kr SMTP
-
-EMAIL_HOST = 'smtp.mokpo.ac.kr'  # 메일 호스트 서버
-EMAIL_PORT = '25'  # 서버 포트
-EMAIL_HOST_USER = 'kakao@mokpo.ac.kr'  # 우리가 사용할 Gmail
-EMAIL_HOST_PASSWORD = 'Eric3371!'  # 우리가 사용할 Gmail p
-EMAIL_USE_TLS = False  # TLS 보안 설정
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER  # 응답 메일 관련 설정
 
 LANGUAGE_CODE = 'ko-kr'
 
@@ -154,4 +207,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
