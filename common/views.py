18bb5384f33609ab02b816301e90d62ad21ca720
clyde0813 <drericcity@gmail.com>
2021-11-20 20:40:45 +0900

    common 대규모 업뎃

--- common/views.py
+++ common/views.py
@@ -1,14 +1,22 @@
-import os
-import random
-
-from django.core.mail import EmailMessage
 from django.shortcuts import render
 
 
 # Create your views here.
+# Create your views here.
+def index(request):
+    return render(request, 'main/index.html')
+
 
 def mypage(request):
-    return render(request, 'common/mypage.html')
+    return render(request, 'common/mypage_info.html')
+
+
+def login(request):
+    return render(request, 'common/login.html')
+
+
+def login_find(request):
+    return render(request, 'common/login_Find.html')
 
 
 def signup(request):
@@ -19,16 +27,27 @@ def page_not_found(request):
     return render(request, '404.html')
 
 
-def email(request):
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Toch.settings.local")
-    otp = str(random.randint(100000, 999999))
-    email = EmailMessage(
-        'Hello',  # 제목
-        otp,  # 내용
-        'toch.text.email@gmail.com',  # 보내는 이메일 (settings에서 설정해서 작성안해도 됨)
-        to=['eric3285@naver.com'],  # 받는 이메일 리스트
-    )
-    email.send()
-    context = {'otp': otp}
-    print(context)
-    return render(request, 'common/email.html', context)
+def usedtrade_chat(request):
+    return render(request, 'common/usedtrade_chat.html')
+
+
+def community_chat(request):
+    return render(request, 'common/community_chat.html')
+
+
+def service(request):
+    return render(request, 'common/mypage_question.html')
+
+# def email(request):
+#     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Toch.settings.local")
+#     otp = str(random.randint(100000, 999999))
+#     email = EmailMessage(
+#         'Hello',  # 제목
+#         otp,  # 내용
+#         'toch.text.email@gmail.com',  # 보내는 이메일 (settings에서 설정해서 작성안해도 됨)
+#         to=['eric3285@naver.com'],  # 받는 이메일 리스트
+#     )
+#     email.send()
+#     context = {'otp': otp}
+#     print(context)
+#     return render(request, 'common/email.html', context)
