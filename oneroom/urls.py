80a63d8a59a34fd831127782ce36cc8f19d1fe0e
DiligentP <qkrwjdgus0603@gmail.com>
2021-11-20 21:08:07 +0900

    restframework needed

--- oneroom/urls.py
+++ oneroom/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:num>', views.detail, name='detail'),
 
-    path('json/',views.json, name='json'),
+    path('json/',views.oneroom_json, name='json'),
 ]
--- oneroom/views.py
+++ oneroom/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.http import JsonResponse
 from django.core import serializers
-
+import json
 from .models import *
 
 
@@ -19,14 +19,13 @@ def detail(request, num):
     return render(request, 'oneroom/detail.html', context)
 
 
-def json(request):
+def oneroom_json(request):
+    Attribution_dataset = Attribution.objects.all().only('id', 'post', 'A', 'B', 'C', 'D', 'E')
+    json_posts = json.dumps(Attribution_dataset)
+    print(json_posts)
+    # queryset_json = serializers.serialize('json', Attribution_dataset)
 
-    Attribution_dataset = Attribution.objects.all()
-
-    queryset_json = serializers.serialize('json',Attribution_dataset)
-
-    context = {'queryset_json':queryset_json}
+    context = {'queryset_json': queryset_json}
 
     return JsonResponse(context)
     # return render(request, 'oneroom/json.html',context)
-
