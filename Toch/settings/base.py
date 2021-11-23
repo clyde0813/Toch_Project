62ea32fbe3d92ef948879c4880403e27c02eda6f
clyde0813 <drericcity@gmail.com>
2021-11-24 03:24:54 +0900

    image fix try 3

--- Toch/settings/base.py
+++ Toch/settings/base.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR) + '/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
