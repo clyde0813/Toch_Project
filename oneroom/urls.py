1600b4983d0e9a386662648f6b4480adb4bbded4
clyde0813 <eric3285@naver.com>
2022-01-08 21:25:14 +0900

    community, usedtrade block

--- oneroom/urls.py
+++ oneroom/urls.py
@@ -6,10 +6,10 @@ from common import views as common_views
 app_name = "oneroom"
 
 urlpatterns = [
-    # path('', views.index, name='index'),
-    # path('<int:num>', views.detail, name='detail'),
-    # path('test/', views.test, name='test'),
+    path('', views.index, name='index'),
+    path('<int:num>', views.detail, name='detail'),
+    path('test/', views.test, name='test'),
 
-    path('', common_views.fixing, name='index'),
-    path('test/', common_views.fixing, name='test'),
+    # path('', common_views.fixing, name='index'),
+    # path('test/', common_views.fixing, name='test'),
 ]
