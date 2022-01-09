26c4273f190d506dbd5aa22732683dfd4f62832d
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-01-09 20:44:05 +0900

    add marker mouse hover

---
+++ static/mobile/js/map.js
@@ -0,0 +1,12 @@
+const kakao_map = document.querySelector('.kakao_map');
+
+ // 지도 중심 좌표 설정
+const MNU_LATITUDE = 34.91124654247272;
+const MNU_LONGITUDE = 126.43614580709644;
+
+const options = {
+    center: new kakao.maps.LatLng(MNU_LATITUDE, MNU_LONGITUDE),
+    level: 5 // 지도의 레벨(확대, 축소 정도)
+};
+
+let map = new kakao.maps.Map(kakao_map, options); //지도 생성 및 객체 리턴
