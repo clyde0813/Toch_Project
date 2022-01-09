f1839668029babe32c33d0f6094050d19dbf2667
clyde0813 <eric3285@naver.com>
2022-01-09 18:57:54 +0900

    oneroom/detail added

--- mobile/urls.py
+++ mobile/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('oneroom/', views.oneroom_index, name='oneroom_index'),
     path('onroom/list', views.oneroom_list, name='oneroom_list'),
+    path('oneroom/detail', views.oneroom_detail, name='oneroom_detail'),
 ]
