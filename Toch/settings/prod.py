10c256c432e399190a891244280e808ca5d927dc
clyde0813 <eric3285@naver.com>
2022-01-10 16:47:23 +0900

    m.toch.kr

--- Toch/settings/prod.py
+++ Toch/settings/prod.py
@@ -1,6 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['114.70.93.100', '192.168.0.106', 'toch.co.kr', 'toch.kr', '.toch.kr']
+ALLOWED_HOSTS = ['114.70.93.100', '192.168.0.106', 'toch.co.kr', 'toch.kr', 'm.toch.kr']
 DEBUG = False
 
 DATABASES = {
