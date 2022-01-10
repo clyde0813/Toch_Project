6af5a39e2b2dd335b1771bff9faa7153d134540a
clyde0813 <eric3285@naver.com>
2022-01-10 16:45:49 +0900

    m.toch.kr

--- Toch/hosts.py
+++ Toch/hosts.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django_hosts import patterns, host
 
-host_patterns = patterns('path.to',
+host_patterns = patterns('',
                          host(r'www', settings.ROOT_URLCONF, name='www'),
                          host(r'm', 'mobile.urls', name='m'),
                          )
