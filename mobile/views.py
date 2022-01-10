38f79c2aefa8a378e95cc601008eae6a7793f821
clyde0813 <eric3285@naver.com>
2022-01-11 01:26:53 +0900

    mobile check-env issue

--- mobile/views.py
+++ mobile/views.py
@@ -5,6 +5,4 @@ from .views_oneroom import *
 
 # Create your views here.
 def index(request):
-    homepage_url = reverse('mobile_oneroom', host='m')
-    context = {'mobile_oneroom_url': homepage_url}
-    return render(request, 'mobile_template/main/index.html', context)
+    return render(request, 'mobile_template/main/index.html')
