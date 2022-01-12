897abdb565e73c35e9b2f7162d8b71559ccf4bc5
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-01-12 16:38:43 +0900

    css & js update

--- static/mobile/js/oneroom-detail-modal.js
+++ static/mobile/js/oneroom-detail-modal.js
@@ -5,19 +5,10 @@ const modal_close_btn = document.querySelector('.modal_close_btn');
 const copy_modal = document.querySelector('.copy_modal');
 
 info_area.addEventListener('click', e => {
-    if(e.target.className ==='contact_btn') {
+    if (e.target.className === 'contact_btn') {
         oneroom_detail_modal_container.classList.remove('none');
     } else if (e.target.className === 'copy_contact_btn') {
-        const contact = document.querySelector('.modal_body').textContent;
-        let textArea = document.createElement("textarea");
-        document.body.appendChild(textArea);
-        textArea.value = contact;
-        textArea.select();
-        document.execCommand('copy');
-        document.body.removeChild(textArea);
-        copy_modal.classList.add('copy_message');
-        setTimeout(() => {copy_modal.classList.remove('copy_message');}, 1250);
-
+        copyContact();
     } else if (e.target.className === 'oneroom_detail_share_btn') {
         console.log('공유');
     }
@@ -27,4 +18,17 @@ modal_close_btn.addEventListener('click', () => {
     oneroom_detail_modal_container.classList.add('none');
 });
 
+function copyContact() {
+    const contact = document.querySelector('.modal_body').textContent;
+    let textArea = document.createElement("textarea");
+    document.body.appendChild(textArea);
+    textArea.value = contact;
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+    copy_modal.classList.add('copy_message');
+    setTimeout(() => {
+        copy_modal.classList.remove('copy_message');
+    }, 1250);
+}
 
