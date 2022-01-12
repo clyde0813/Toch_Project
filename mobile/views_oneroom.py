aa0d5fca0730cf51dda4bf8625d3ee8dd8b06ef2
clyde0813 <eric3285@naver.com>
2022-01-12 14:05:13 +0900

    mobile oneroom Contact added

--- mobile/views_oneroom.py
+++ mobile/views_oneroom.py
@@ -6,8 +6,9 @@ from ip_gather import get_client_ip
 
 
 def oneroom_index(request):
+    contact = Contact.objects.all()
     data = Post.objects.all().annotate(num_like=Count('like_set')).order_by('-num_like')
-    context = {'data': data}
+    context = {'data': data, 'contact': contact}
     return render(request, 'mobile_template/oneroom/oneroom.html', context)
 
 
